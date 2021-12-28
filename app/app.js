fb3e0a5fda42f8c05eb81e6634bf9c362b4ad7e7
daichihasegawa0923 <daichihasegawa052@gmail.com>
2021-12-29 08:52:45 +0900

    add ejs file

--- app/app.js
+++ app/app.js
@@ -1,10 +1,23 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+const url = require('url');
+
+const pageContent = fs.readFileSync('./pages/index.ejs', 'utf-8');
+const cssContent  = fs.readFileSync('./styles/main.css', 'utf-8');
 
 var server =  http.createServer(
     (req,res)=>{
-        const htmlFileContent = fs.readFileSync('./pages/index.html');
-        res.end(htmlFileContent);
+        console.log(req.url);
+        var urlParts = req.url;
+        switch(urlParts.pathname){
+            case '/':
+                var content = ejs.render(pageContent, {userName: 'UserName'});
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(content);
+                res.end();
+                break;
+        }
     }
 );
 
